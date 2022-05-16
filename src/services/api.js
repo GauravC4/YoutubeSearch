b06d604132902d68ecf217876b67950f3206a73a
Gaurav Chemburkar <gauravchemburkar@plexusmd.com>
2022-05-16 10:28:12 +0530

    key added

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,11 @@ const instance = axios.create({
   timeout: 1000,
 });
 
+const key_1 = "AIzaSyBcwbOynZiqlowkJDNlZc4hFrJKg47BXS0";
+const key_2 = "AIzaSyAbaF84n4ay6fy3_KHAbb3VK46fwAT9-qs";
+
 instance.defaults.params = {};
-instance.defaults.params["key"] = "AIzaSyBcwbOynZiqlowkJDNlZc4hFrJKg47BXS0";
+instance.defaults.params["key"] = key_2;
 instance.defaults.params["maxResults"] = "10";
 
 export default instance;
